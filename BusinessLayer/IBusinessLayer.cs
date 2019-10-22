dca6932d370a79752b3eb32b834e433410daecbe
unknown <baiza.christian@gmail.com>
2019-10-22 10:17:35 -0700

    Init Commit

---
+++ BusinessLayer/IBusinessLayer.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public interface IBusinessLayer
+    {
+        #region Standard
+        IEnumerable<Standard> GetAllStandards();
+
+        Standard GetStandardByID(int id);
+
+        Standard GetStandardByName(string name);
+
+        void AddStandard(Standard standard);
+
+        void UpdateStandard(Standard standard);
+
+        void RemoveStandard(Standard standard);
+        #endregion
+
+        #region Student
+        IList<Student> GetAllStudents();
+        Student GetStudentByID(int id);
+        Student GetStudentByName(string name);
+        void AddStudent(Student student);
+        void UpdateStudent(Student student);
+        void RemoveStudent(Student student);
+        
+        #endregion
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+
+namespace Client
+{
+    class Program
+    {
+        //----------------------------------------- Slots ------
+        static BusinessLayer.BusinessLayer bl = new BusinessLayer.BusinessLayer();
+
+
+        SearchStandardStudents();
+        //            break;
+
+
+        //------------------- SearchStandardStudents ------
+        private static void SearchStandardStudents()
+        {
+            Console.Write("Enter Standard ID: ");
+            ReadChoice();
+            Standard s = bl.GetStandardByIDWithStudents(choice);
+            if (s == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            else if (s.Students == null || s.Students.Count == 0)
+            {
+                Console.WriteLine("This Standard has no Students!");
+                return;
+            }
+
+            Console.WriteLine("\nContaining Students: {0}", s.Students.Count);
+            foreach (Student student in s.Students)
+                Console.WriteLine("- " + student.StudentName);
+        }
+    }
+}
